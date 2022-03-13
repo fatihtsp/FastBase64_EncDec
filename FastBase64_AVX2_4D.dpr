3bfb73297d3fbc2678636d5b050a57a686a0c1d0
Fatih Taspınar <fatihtsp@gmail.com>
2022-03-13 14:09:58 +0300

    Initial changes, project setup and first commit
    Initial changes, command line project setup and first commit

---
+++ FastBase64_AVX2_4D.dpr
@@ -0,0 +1,17 @@
+program FastBase64_AVX2_4D;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
